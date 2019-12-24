2d363d597fce3b39b72b7517871b9d5ce57a3f5a
松本世 <matumoto.p04a@gmail.com>
2019-12-24 08:36:20 +0530

    first

---
+++ Desktop/Django/ecsite/users/admin.py
@@ -0,0 +1,41 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm,UserCreationForm
+from django.utils.translation import ugettext_lazy as _
+from .models import User
+
+class MyUserChangeFrom(UserChangeForm):
+    #User情報を変更するフォーム
+    class Meta:
+        models=User
+        fields='__all__'
+
+class MyUserCreationForm(UserCreationForm):
+    #Userを作成するフォーム
+    class Meta:
+        model=User
+        fields=('email',)#emailとパスワードが必要
+
+class MyUserAdmin(UserAdmin):
+    #カスタムユーザーモデルのAdmin
+    fieldsets=(
+        (None,{'fields':('email','password')}),
+        (_('permissions'),{'fields':('is_active','is_staff','is_superuser','groups','user_parmissions')}),
+        (_('Inportant datas'),{'fields':('last_login','date_joined')}),
+    )
+    add_fieldsets=(
+        (None,{
+            'class':('wide',),
+            'fields':('email','password1','password2'),
+        }),
+    )
+    form=MyUserChangeFrom
+    add_form=MyUserCreationForm
+    list_display=('email','is_staff')
+    list_filter=('is_staff','is_superuser','is_active','groups')
+    search_fields=('emal',)
+    ordering=('email',)
+
+admin.site.register(User,MyUserAdmin)
