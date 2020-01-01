9d8e87f37220ea8123547e1e594dc2feb59eea73
松本世 <matumoto.p04a@gmail.com>
2020-01-01 21:46:47 +0530

    forth

--- Desktop/Django/done/photoservice5/photoservice5/settings.py
+++ Desktop/Django/done/photoservice5/photoservice5/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'photoservice5.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER': 'user_name',
+        'PASSWORD': 'password',
     }
 }
 
